7fe10143c7aaa48267d6a642dc20cc4e8f1e6273
EdmanUVG <54090748+EdmanUVG@users.noreply.github.com>
2020-02-03 12:11:04 -0600

    Agregar metedos

--- CalculadoraPostfix/src/Calculadora.java
+++ CalculadoraPostfix/src/Calculadora.java
@@ -1,61 +1,88 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Calculadora {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Stack<Integer> miStack = new StackArrayList<Integer>();
 
-		String expresiones = "6 2 3 + *";
-
-		Integer operandoB = 0;
-		Integer operandoA = 0;
-
-		Integer result = 0;
-
-		String[] opercionArray = expresiones.split(" ");
-
-		// print elements of String array
-		for (int i = 0; i < opercionArray.length; i++) {
-
-			if (opercionArray[i].equals("+")) {
-				
-				operandoA = miStack.pop();
-				operandoB = miStack.pop();
-				
-				result = operandoA + operandoB;
-				miStack.push(result);
-				
-			} else if (opercionArray[i].equalsIgnoreCase("-")) {
-				
-				operandoA = miStack.pop();
-				operandoB = miStack.pop();
-				
-				result = operandoA - operandoB;
-				miStack.push(result);
-				
-			} else if (opercionArray[i].equalsIgnoreCase("*")) {
-				
-				operandoA = miStack.pop();
-				operandoB = miStack.pop();
-
-				result = operandoA * operandoB;
-				miStack.push(result);
-
-			} else if (opercionArray[i].equalsIgnoreCase("/")) {
-				
-				operandoA = miStack.pop();
-				operandoB = miStack.pop();
-				
-				result = operandoA / operandoB;
-				miStack.push(result);
-				
-			} else {
-				Integer num = Integer.parseInt(opercionArray[i]);
-				miStack.push(num);
-			}
-		}
 		
-		System.out.println(miStack.pop());
+
+		int operandoB = 0;
+		int operandoA = 0;
+
+		int result = 0;
+		
+		
+		// Busca y carga el archivo datos.txt donde se guardan los datos.
+		BufferedReader in = new BufferedReader(new FileReader("src/datos.txt"));
+		
+		// Variable para guardar uno a uno los datos sacados del txt
+		String expresion;
+		
+		// Corre mientras hay datos para leer
+		while((expresion = in.readLine()) != null)
+		{
+			// Se divide la cadena en arreglos para mas facil la busqueda letra por letra
+			String[] opercionArray = expresion.split(" ");
+
+			// Recorre todos los elementos de del arreglo
+			for (int i = 0; i < opercionArray.length; i++) {
+				
+				// Si el elemento es un signo + entonces es una suma
+				if (opercionArray[i].equals("+")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					
+					result = operandoA + operandoB;
+					miStack.push(result);
+					
+					// Si el elemento es un signo - entonces es una resta
+				} else if (opercionArray[i].equalsIgnoreCase("-")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					
+					result = operandoA - operandoB;
+					miStack.push(result);
+					
+					// Si el elemento es un signo * entonces es una multiplicacion
+				} else if (opercionArray[i].equalsIgnoreCase("*")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+
+					result = operandoA * operandoB;
+					miStack.push(result);
+
+					// Si el elemento es un signo / entonces es una division
+				} else if (opercionArray[i].equalsIgnoreCase("/")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					
+					result = operandoA / operandoB;
+					miStack.push(result);
+					
+					// Si el elemento es un numero, entonces se agrega al stack
+				} else {
+					Integer num = Integer.parseInt(opercionArray[i]);
+					miStack.push(num);
+				}
+			}
+			// Al final de correr en cada elemento se despliega el resultado de las operaciones
+			System.out.println(miStack.pop());
+		}
+		in.close(); // Cierra el BufferReader cuando finalizamos en buscar todos los datos
+		
+		
+		
+		
+		
 
 	}
 
