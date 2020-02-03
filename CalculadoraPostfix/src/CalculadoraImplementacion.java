7ff1c467d0ee6369363e90dfd0a3df7608a4df3e
EdmanUVG <54090748+EdmanUVG@users.noreply.github.com>
2020-02-03 12:09:55 -0600

    Agregar parametros

--- CalculadoraPostfix/src/CalculadoraImplementacion.java
+++ CalculadoraPostfix/src/CalculadoraImplementacion.java
@@ -1,28 +1,33 @@
 
 public class CalculadoraImplementacion implements CalculadoraInterface {
-
+	
+	int operandoA = 0;
+	int operandoB = 0;
+	
 	@Override
-	public void sumar() {
+	public int sumar(int paramA, int paramB) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
 
 	@Override
-	public void restar() {
+	public int restar(int paramA, int paramB) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
 
 	@Override
-	public void multiplicar() {
+	public int multiplicar(int paramA, int paramB) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
 
 	@Override
-	public void dividir() {
+	public int dividir(int paramA, int paramB) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
+
+	
 
 }
