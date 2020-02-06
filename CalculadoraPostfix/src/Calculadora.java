5429251b0071ddf308c49710f83942ca01cb29e3
Oliver Josué De León Milian <46546727+Olivverde@users.noreply.github.com>
2020-02-05 23:15:14 -0600

    Final Commit

--- CalculadoraPostfix/src/Calculadora.java
+++ CalculadoraPostfix/src/Calculadora.java
@@ -6,109 +6,114 @@
  * 
  */
 
+
+//Lista de importaciones
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Calculadora {
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 */
 	public static void main(String[] args) throws IOException {
 
-		// Stack generico. Acepta cualquier tipo de dato
+		//Embajadores de clases
 		Stack<Integer> miStack = new StackArrayList<Integer>();
-
-		// Implementa los metods de nuestra interfaz de calculadora
-		CalculadoraImplementacion implementacion = new CalculadoraImplementacion();
-
-		// Se guarda los resultdos de las operaciones
-		int result = 0;
-
-
-		// Busca y carga el archivo datos.txt donde se guardan los datos.
-		BufferedReader in = new BufferedReader(new FileReader("src/datos.txt"));
-
-		// Variable para guardar uno a uno los datos sacados del txt
+		CalculadoraImplementacion calculadora = new CalculadoraImplementacion();
+		
+		//Atributos
+		
+		int operandoB;
+		int operandoA;
+		int result;
 		String expresion;
-
+		ArrayList<String> operacionArray = new ArrayList<String>();
+		
+		
+		// Busca y carga el archivo datos.txt donde se guardan los datos.
+		BufferedReader in = new BufferedReader(new FileReader("src/calculadora/datos.txt"));
+		
 		// Corre mientras hay datos para leer
 		while((expresion = in.readLine()) != null)
 		{
-			// Se divide la cadena en arreglos para mas facil la busqueda letra por letra
-			String[] opercionArray = expresion.split("");
-
+			
+			// Se separa cada componente del String y se
+			//guarda en un ArrayList
+			for(int s = 0; s < expresion.length(); s++) {
+				String cut = expresion.substring(s,s+1);
+				operacionArray.add(cut);
+			}
+			
+			int a = 0;
 			// Recorre todos los elementos de del arreglo
-			for (int i = 0; i < opercionArray.length; i++) {
+			for (int i = 0; i < operacionArray.size(); i++) {
+				a = 0;
+				
+				// Si el elemento es un signo "+" entonces es una suma
+				if (operacionArray.get(i).equalsIgnoreCase("+")) {
+					
+					//Se eliminan los operadores del stack
+					//mientras retorna el valor de cada uno
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					//calcula resultado
+					result = calculadora.sumar(operandoA, operandoB);
+					//Sube el resultado al stack
+					miStack.push(result);
+					
+				// Si el elemento es un signo "-" entonces es una resta
+				} else if (operacionArray.get(i).equalsIgnoreCase("-")) {
+					
+					//""
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					result = calculadora.restar(operandoA, operandoB);
+					
+					miStack.push(result);
+					
+					// Si el elemento es un signo "*" entonces es una multiplicacion
+				} else if (operacionArray.get(i).equalsIgnoreCase("*")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					result = calculadora.multiplicar(operandoA, operandoB);
 
-				// Si el elemento es un signo + entonces es una suma
-				if (opercionArray[i].equals("+")) {
-
-					// Retorna el resultado de la operacion y lo guarda
-					// @param1 = miStack.pop()
-					// @param2 = miStack.pop()
-					result = implementacion.sumar(miStack.pop(), miStack.pop());
-
-					// Se agrega el resultado a la ultima posicion del stack
+					result = operandoA * operandoB;
 					miStack.push(result);
 
-					// Si el elemento es un signo - entonces es una resta
-				} else if (opercionArray[i].equalsIgnoreCase("-")) {
-
-					// Retorna el resultado de la operacion y lo guarda
-					// @param1 = miStack.pop()
-					// @param2 = miStack.pop()
-					result = implementacion.restar(miStack.pop(), miStack.pop());
-
-					// Se agrega el resultado a la ultima posicion del stack
-					miStack.push(result);
-
-					// Si el elemento es un signo * entonces es una multiplicacion
-				} else if (opercionArray[i].equalsIgnoreCase("*")) {
-
-					// Retorna el resultado de la operacion y lo guarda
-					// @param1 = miStack.pop()
-					// @param2 = miStack.pop()
-					result = implementacion.multiplicar(miStack.pop(), miStack.pop());
-
-					// Se agrega el resultado a la ultima posicion del stack
-					miStack.push(result);
-
-					// Si el elemento es un signo / entonces es una division
-				} else if (opercionArray[i].equalsIgnoreCase("/")) {
-
-					// Retorna el resultado de la operacion y lo guarda
-					// @param1 = miStack.pop()
-					// @param2 = miStack.pop()
-					result = implementacion.dividir(miStack.pop(), miStack.pop());
-
-					// Se agrega el resultado a la ultima posicion del stack
-					miStack.push(result);
-
+					// Si el elemento es un signo "/" entonces es una division
+				} else if (operacionArray.get(i).equalsIgnoreCase("/")) {
+					
+					operandoA = miStack.pop();
+					operandoB = miStack.pop();
+					if(operandoB != 0) {
+						result = calculadora.dividir(operandoA, operandoB);
+						miStack.push(result);
+					}else {
+						a += 1;
+					}
+					
 					// Si el elemento es un numero, entonces se agrega al stack
 				} else {
 					try {
-
-						// Se convierte el String a enteros
-						Integer num = Integer.parseInt(opercionArray[i]);
-
-						// Se agrega el entero al stack
+						Integer num = Integer.parseInt(operacionArray.get(i));
 						miStack.push(num);
-
-					} catch(Exception e) {
-						//						System.out.println("Operando....");
+					}catch(Exception e) {
+						
 					}
-
+					
 				}
 			}
 			// Al final de correr en cada elemento se despliega el resultado de las operaciones
-			System.out.println(String.format("%-20s", expresion) + " = "  + miStack.pop());
+			if (a == 0) {
+				System.out.println("El resultado de la operacion es: "+miStack.pop());
+			}else {
+				System.out.println("ERROR: operacion invalida");
+			}
+			
+			
 		}
-		in.close(); // Cierra el BufferReader cuando finalizamos en buscar todos los datos
-
+		in.close(); // Cierra el BufferReader cuando finalizamos en buscar todos los datos	
 	}
-
 }
